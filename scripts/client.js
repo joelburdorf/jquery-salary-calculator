2f3281170f1e9dfa89060aa91637cc8b8aa2dc89
Joel Burdorf <joelburdorf@Joels-MacBook-Pro.local>
2020-01-31 17:21:52 -0600

    added addEmpInfo function

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,25 @@
 console.log('in js');
+
+$(document).ready(onReady);
+
+//employee app
+let employees = [];
+
+function onReady() {
+    console.log('in onReady');
+    $('#addInfoButton').on('click', addEmpInfo);
+   // $('#songsOut').on('click', '.digButton', digSong);
+} //end onReady
+
+function addEmpInfo(){
+    console.log('in addEmpInfo', employees);
+}
+
+
+employees.push( {
+    firstName: 'John',
+    lastName: 'Doe',
+    id: 222,
+    title: 'Engineer',
+    annualSalery: 5000
+});
