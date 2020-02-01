de574b39bae2e4abe7985030a25913994758499f
Joel Burdorf <joelburdorf@Joels-MacBook-Pro.local>
2020-01-31 18:07:51 -0600

    comleted displayEmployee function, able to push info to DOM

--- scripts/client.js
+++ scripts/client.js
@@ -13,8 +13,40 @@ function onReady() {
 
 function addEmpInfo(){
     console.log('in addEmpInfo', employees);
+    // sellect inputs by their ID 
+    //use their data in a newObj (using .val as a "getter")
+    let newObj = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        id: $('#idIn').val(),
+        title: $('#titleIn').val(),
+        annualSalery: $('#annualSaleryIn').val()
+    }
+        //empty inputs (using .val as a "setter")
+        $('#firstNameIn').val('');
+        $('#lastNameIn').val('');
+        $('#idIn').val('');
+        $('#titleIn').val('');
+        $('#annualSaleryIn').val('');
+        //push new emp object into employees array
+        employees.push( newObj);
+        //display employeeInfo
+        displayEmployee();
 }
 
+function displayEmployee (){
+    console.log('in displayEmployee');
+    //target an output element by ID
+    let el = $('#empOut');
+    //empty out el
+    el.empty();
+    //loop through employees array
+    for (let i=0; i<employees.length; i++){
+        //append each employee to the DOM
+        el.append(`<li> ${employees[i].firstName} ${employees[i].lastName}, ${employees[i].id}, ${employees[i].title}, 
+        ${employees[i].annualSalery} </li>`)
+    }//end for
+}//end displayEmployee
 
 employees.push( {
     firstName: 'John',
